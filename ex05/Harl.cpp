30751dbd277f9aed802269a12e714d86a10fd98c
pauldahacker <paul.demasca@gmail.com>
2024-11-20 18:39:16 +0100

    mandatory

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -12,22 +12,22 @@ Harl::~Harl(void)
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "This is Harl's DEBUG message!" << std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "This is Harl's INFO message!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "This is Harl's WARNING message!" << std::endl;
 }
 
 void Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "This is Harl's ERROR message!" << std::endl;
 }
 
 void Harl::complain(std::string level)
